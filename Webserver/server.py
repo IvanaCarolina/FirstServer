180b9fd77d7ed8027ee0ef981d4c723592bc9da4
Ivana Carolina <75748698+IvanaCarolina@users.noreply.github.com>
2021-06-21 20:25:51 -0300

    editando

--- Webserver/server.py
+++ Webserver/server.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def homescreen():
     return render_template('index.html')
 
-@app.route('/about')#
-def about():
-    return render_template('index2.html')
+# @app.route('/<username>/<int:post_id>')
+# def user(username=None, post_id=None):
+#     return render_template('index.html', name=username, post_id=post_id)
 
-@app.route('/blog')
-def blog():
-    return 'Thinking about starting a blog.'
+@app.route('/index.html')
+def home():
+    return render_template('index.html')
+
+@app.route('/project.html')
+def project():
+    return render_template('project.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/components.html')
+def components():
+    return render_template('components.html')

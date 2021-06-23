96c55c1979f973a167b4d359ae17014f5593b19d
Ivana Carolina <75748698+IvanaCarolina@users.noreply.github.com>
2021-06-23 14:05:39 -0300

    almost done

--- Webserver/server.py
+++ Webserver/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -21,6 +21,15 @@ def project():
 def about():
     return render_template('about.html')
 
-@app.route('/components.html')
-def components():
-    return render_template('components.html')
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    # error = None
+    # if request.method == 'POST':
+    #     if valid_login(request.form['username'],
+    #                    request.form['password']):
+    #         return log_the_user_in(request.form['username'])
+    #     else:
+    #         error = 'Invalid username/password'
+    # # the code below is executed if the request method
+    # # was GET or the credentials were invalid
+    return render_template('login.html', error=error)

28f915f5cf90c8ef1e64891fb7262aa5bc12c6c6
Ivana Carolina <75748698+IvanaCarolina@users.noreply.github.com>
2021-06-24 22:33:52 -0300

    links

--- Webserver/README.md
+++
@@ -1,1 +0,0 @@
-# FirstServer
--- Webserver/server.py
+++ Webserver/server.py
@@ -21,15 +21,15 @@ def project():
 def about():
     return render_template('about.html')
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    # error = None
-    # if request.method == 'POST':
-    #     if valid_login(request.form['username'],
-    #                    request.form['password']):
-    #         return log_the_user_in(request.form['username'])
-    #     else:
-    #         error = 'Invalid username/password'
-    # # the code below is executed if the request method
-    # # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#      error = None
+#      if request.method == 'POST':
+#          if valid_login(request.form['username'],
+#                         request.form['password']):
+#              return log_the_user_in(request.form['username'])
+#          else:
+#              error = 'Invalid username/password'
+#the code below is executed if the request method
+#was GET or the credentials were invalid
+#     return render_template('login.html', error=error)
